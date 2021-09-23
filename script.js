809cf896baade5f95137bac4e8d60a13fd9554a7
Erik Gustuson <gusgus1919@gmail.com>
2021-09-23 01:31:15 -0700

    add starting variables for character sets

--- script.js
+++ script.js
@@ -8,20 +8,17 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
 
+//define what characters are to be used for each selection
 function generatePassword() {
-  var uppercase
+  var characters = ""
+  var lowercases = "abcdefghijklmnopqrstuvwxyz"
+  var uppercases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var numbers = "0123456789"
+  var specials = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
 
 
 
-return "password"
-}
-
-// TODO: define what characters are to be used for each selection
-// TODO: check to see how many characters are to be used 8 to 128
-// TODO: create propts so that user can go though and select characters wanted
-//        choices lowercase, uppercase, numeric, and/or special characters
 // TODO: confirm that at lease one character type has been selected
 // TODO: after all criteria is selected a password should be generated and displayed 
 //        in an alert or written to the page
