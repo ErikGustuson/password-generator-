15536f29ab47dd0a9b250586993bb9ff302a0ece
Erik Gustuson <gusgus1919@gmail.com>
2021-09-23 02:03:39 -0700

    add confirm statements for different characters

--- script.js
+++ script.js
@@ -19,21 +19,42 @@ function generatePassword() {
 
 // check to see how many characters are to be used 8 to 128
   var characterCount = prompt("How many characters would you like in your password?", "12")
-  if (characterCount < 8 || characterCount > 128 || false) {
+  if (characterCount < 8 || characterCount > 128) {
     alert("Please select a number from 8 to 128")
-    generatePassword()
   } 
 
 
+// create propts so that user can go though and select characters wanted
+//        choices lowercase, uppercase, numeric, and/or special characters
+  if (confirm("Would you like any lowercase letters?") == true) {
+    characters = characters.concat(lowercases)
+  } 
+  if (confirm("Would you like any uppercase letters?") == true) {
+    characters = characters.concat(uppercases)
+  }
+  if (confirm("Would you like any numbers?") == true) {
+    characters = characters.concat(numbers)
+  }
+  if (confirm("Would you like special characters?") == true) {
+    characters = characters.concat(specials)
+  }
 
+ 
+  }
+ // after all criteria is selected a password should be generated and displayed 
+ // in an alert or written to the page
+
+ // Used console.log to confrom that password was getting generated correctly
+  console.log(password)
+
+return password
 
 }
 }
 
 
-// TODO: confirm that at lease one character type has been selected
-// TODO: after all criteria is selected a password should be generated and displayed 
-//        in an alert or written to the page
+
+        
 
 
 
